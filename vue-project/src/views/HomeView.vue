132407391831ece850cb8c2815d6b9db9a92c3a1
juuclue <144699439+juuclue@users.noreply.github.com>
2024-04-09 09:51:32 -0400

    Revert "Merge branch 'jenny' into amy"
    This reverts commit 698a4ec32f8263e3260b90522bed57736d7c917e, reversing
    changes made to 0863e09275f044a811cf8e5f0df3d7b7788614c0.

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,6 +1,9 @@
-<template>
-
-</template>
-
-<script>
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
