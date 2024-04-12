7cb930014bc6b0424532a6a160c24fe5db4395e8
Kelly Ng <kellyng19lover@gmail.com>
2024-04-12 10:15:12 -0400

    f
    fg

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
-<div>
-  <form name="login-form" @submit.prevent="login(username, password)">
-      <div>
-          <label for="username">Username: </label>
+    <div>
+      <form name="login-form" @submit.prevent="login(username, password)">
+        <div>
+          <label ref="input-user" for="username">Username: </label>
           <input id="username" type="text" v-model="username">
-      </div>
-      <div>
-          <label for="password">Password: </label>
+        </div>
+        <div>
+          <label ref="input-pass" for="password">Password: </label>
           <input id="password" type="text" v-model="password">
-      </div>
-          <button class="submit-button" type="submit"> Login </button>
-  </form>
-</div>
+        </div>
+        <button @click="clear()" type="submit"> Login </button>
+      </form>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script>
+import { ref } from 'vue';
 
+const password = ref ("");
+const username = ref ("");
+const input = ref("");
+
+console.log(input)
+async function login(username,){
+
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
