bc8e5497f60c1fd55632a14d41e41b08413f6294
amyl164 <amyl81008@gmail.com>
2024-04-17 10:07:45 -0400

    hre

--- vue-project/src/components/SignUp.vue
+++ vue-project/src/components/SignUp.vue
@@ -9,14 +9,25 @@
           <label for="password">Password: </label>
           <input id="password" type="text" v-model="password">
       </div>
-          <button class="submit-button" type="submit"> Login </button>
+          <!-- <button @click=" add(username)"class="submit-button" type="submit"> Login </button> -->
   </form>
     </div>
 </template>
 
 <script setup>
-
-
+// import { reactive } from 'vue'
+// const add = function(x){
+//   form.username.push(x)
+// }
+// const form = reactive({
+//     username: "",
+//     password: "",
+// })
+// import supabase from '@/lib/supabaseClient.js'
+// const { error } = await supabase
+//   .from('Users')
+//   .insert({ id: 1, Username: username.value})
+// error
 </script>
 
 <style scoped>
