4b900dd8f6109e4d354bf35924c96496baa7db94
Kelly Ng <kellyng19lover@gmail.com>
2024-04-18 09:56:55 -0400

    io;
    i;

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,45 +1,13 @@
 <template>
   <div>
-    <form @submit.prevent="Submit">
-      <label>Username</label>
-      <input type="text" required v-model="user.name" id="name">
-      <label>Password</label>
-      <input type="password" required v-model="user.pass" id="pass">
-    </form>
-
-    <button type="submit" class=buttonSignup @click="Submit">Submit</button>
-    <br/>
-    You typed:<br/>
-    Username: {{user.name}}<br/>
-    Password: {{user.pass}}<br/>
 
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      users: [],
+<script setup>
 
-      user: {
-        name: '',
-        pass: '',
-        number: ''
-      }
-    };
-  },
-  methods: {
-
-    Submit() {
-      // do some checking here
-      this.users.push(this.user)
-      this.user = {name:'', pass:'',};
-      console.log(this.users)
-
-    },
-
-
-  }
-}
 </script>
+
+<style lang="scss" scoped>
+
+</style>
