716d3cb1bccf270e56e09e1bab4933a74347eac8
amyl164 <amyl81008@gmail.com>
2024-04-17 20:22:59 -0400

    grse

--- vue-project/src/components/SignUp.vue
+++ vue-project/src/components/SignUp.vue
@@ -1,45 +1,68 @@
 <template>
-    <div>
+    <div class="header">
       <form @submit.prevent="Submit">
         <label>Username</label>
-        <input type="text" required v-model="user.name" id="name">
+        <input type="text" required v-model="user.Username" id="name">
         <label>Password</label>
-        <input type="password" required v-model="user.pass" id="pass">
+        <input type="password" required v-model="user.Password" id="pass">
       </form>
   
       <button type="submit" class=buttonSignup @click="Submit">Submit</button>
       <br/>
       You typed:<br/>
-      Username: {{user.name}}<br/>
-      Password: {{user.pass}}<br/>
+      Username: {{user.Username}}<br/>
+      Password: {{user.Password}}<br/>
   
     </div>
   </template>
   
   <script>
+  import { createClient } from '@supabase/supabase-js'
+export const supabase = createClient('https://dctworfsniezubogathx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdHdvcmZzbmllenVib2dhdGh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyMzk3ODEsImV4cCI6MjAyNzgxNTc4MX0.3TNwmbvfuxmSan_Ma7J7SyF7X100nqkxp5v_DNimGGo')
+
   export default {
     data() {
       return {
         users: [],
   
         user: {
-          name: '',
-          pass: '',
-          number: ''
+          Username: '',
+          Password: '',
         }
       };
     },
     methods: {
   
       Submit() {
-        // do some checking here
         this.users.push(this.user)
-        this.user = {name:'', pass:'',};
+        this.user = {Username:'', Password:'',};
         console.log(this.users)
+        this.users.forEach((user) => {
+        supabase.from('Users').insert([user])
+            .then(({ data, error }) => {
+                if (error) {
+                    console.error(error.message);
+                } else {
+                    console.log(data);
+                }
+            });
+    });
   
       },
-  
-  
     }
   }
+  
+
   </script>
+
+  
+  <style scoped>
+  .header{
+    flex: auto;
+    align-items: center;
+    font-size: 2rem;
+    text-align: center;
+    padding-top: 10rem;
+  }
+  
+  </style>
