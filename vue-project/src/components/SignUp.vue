2ec9acf20df5285b24d9ce081923e3a2d623ecf1
amyl164 <amyl81008@gmail.com>
2024-04-19 10:14:58 -0400

    efw

--- vue-project/src/components/SignUp.vue
+++ vue-project/src/components/SignUp.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="header">
       <form @submit.prevent="Submit">
+        <label>Email</label>
+        <input type="text" required v-model="user.Email" id="namee">
         <label>Username</label>
         <input type="text" required v-model="user.Username" id="name">
         <label>Password</label>
@@ -18,6 +20,7 @@
   </template>
   
   <script>
+  
 import {supabase} from '@/lib/supabaseClient.js'
 
   export default {
@@ -26,6 +29,7 @@ import {supabase} from '@/lib/supabaseClient.js'
         users: [],
   
         user: {
+          Email:'',
           Username: '',
           Password: '',
         }
@@ -35,23 +39,50 @@ import {supabase} from '@/lib/supabaseClient.js'
   
      Submit() {
         this.users.push(this.user)
-        this.user = {Username:'', Password:'',};
+        this.user = {Email:'',Username:'', Password:'',};
         console.log(this.users)
         this.users.forEach(async(user) => {
           let { data, error } = await supabase.auth.signUp({
-  email: user.Username,
-  password: user.Password
-    });
+  email: user.Email,
+  password: user.Password,
+  options: {
+    data: {
+      Username: user.Username,
+    }}});
 if (error) {
   console.error(error.message);
    } else {
       console.log(data);
+      
                     
      }
      },
     )
+  
     document.querySelector("h3").textContent = ("You have signed up! Click login in to open app");
-  }}}
+ 
+  }
+}}
+Submit.then();{
+
+}
+// -- inserts a row into public.profiles
+// create function public.handle_new_user()
+// returns trigger
+// language plpgsql
+// security definer set search_path = public
+// as $$
+// begin
+//   insert into public.profiles (id, first_name, age)
+//   values (new.id, new.raw_user_meta_data ->> 'first_name', new.raw_user_meta_data['age']::integer);
+//   return new;
+// end;
+// $$;
+
+// -- trigger the function every time a user is created
+// create trigger on_auth_user_created
+//   after insert on auth.users
+//   for each row execute procedure public.handle_new_user();
 
   //       supabase.from('Users').insert([user])
   //           .then(({ data, error }) => {
