6865bdc6b164063737423956fdf086f7bc458fbe
amyl164 <amyl81008@gmail.com>
2024-05-09 09:40:07 -0400

    gr

--- vue-project/src/components/SignIn.vue
+++ vue-project/src/components/SignIn.vue
@@ -1,4 +1,14 @@
 <template>
+   <header>
+      <nav>
+         <RouterLink class="" to="/"></RouterLink >
+
+         <RouterLink class="navigate"to="/signup">Sign Up</RouterLink>
+         <RouterLink class="navigate"to="/signin">Sign In</RouterLink>
+   
+      </nav>
+  </header>
+  <RouterView />
     <div class="header">
       <form @submit.prevent="Submit" class="form">
         <div class="form1">
@@ -12,16 +22,12 @@
         <button type="submit" class="button">Submit</button>
       </form>
   <h3></h3>
-  <div v-if="loggedIn">
-      <UserInfo />
-    </div>
     </div>
   </template>
   
   <script>
   import { supabase } from '@/lib/supabaseClient.js'
-  import UserInfo from '@/components/UserInfo.vue'
-  
+  import { RouterLink, RouterView } from 'vue-router'
   export default {
     data() {
       return {
@@ -60,6 +66,26 @@
   
   
   <style scoped>
+  header {
+  font-size: 2rem;
+  display: flex;
+  justify-content: space-between; 
+  align-items: center;
+  padding: 10px 20px; 
+}
+.navigate{
+  padding: 10px 20px;
+  margin-right: 10px; 
+  background-color:rgb(57, 188, 231);
+  color: white; 
+  border-radius: 5px; 
+  cursor: pointer; 
+  text-decoration: none; 
+  transition: background-color 0.3s; 
+}
+.navigate:hover {
+  background-color: rgb(138, 198, 218); 
+}
 .header {
   display: flex;
   flex-direction: column;
