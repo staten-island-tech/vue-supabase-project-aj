cf0f3c3bc29c2f2d9c60ed4380ad1ef496cb8648
amyl164 <amyl81008@gmail.com>
2024-05-09 10:15:24 -0400

    gvw

--- vue-project/src/components/WelcomeView.vue
+++ vue-project/src/components/WelcomeView.vue
@@ -1,13 +1,57 @@
 <template>
-    <div>
-<h1>Hi</h1>
+  <div class="header">
+    <form @submit.prevent="Submit" class="form">
+      <div class="form1">
+        <label for="namee">Username</label>
+        <input type="text" required v-model="user.Username" id="namee" class="form2">
+      </div>
+      <button type="submit" class="button">Submit</button>
+    </form>
     </div>
 </template>
 
-<script setup>
+<script>
+import {supabase} from '@/lib/supabaseClient.js'
+export default {
+  data() {
+    return {
+      users: [],
+
+      user: {
+        Username: '',
+      }
+    };
+  },
+  methods: {
+
+    Submit() {
+      this.users.push(this.user)
+      this.user = {Username:'',};
+      console.log(this.users)
+      this.users.forEach(async(user) => {
+          let { data, error } = await supabase.from('profiles')
+          .update({Username: user})
+          if (error) {
+  console.error(error.message);
+   } else {
+      console.log(data);        
+     }});
+    }}}
+         
+
 
 </script>
 
 <style scoped>
 
 </style>
+<!-- 
+this.users.forEach((user) => {
+    supabase.from('profiles').update({Username: user})
+        .then(({ data, error }) => {
+            if (error) {
+                console.error(error.message);
+            } else {
+                console.log(data);
+            }
+        }); -->
