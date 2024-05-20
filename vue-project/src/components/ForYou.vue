b8083bf4236c5f58751ee32c65b7aaef95d39fd6
amyl164 <amyl81008@gmail.com>
2024-05-20 10:14:56 -0400

    sas

--- vue-project/src/components/ForYou.vue
+++ vue-project/src/components/ForYou.vue
@@ -13,11 +13,54 @@
     </div>
       <div>
           <h1 class="page">FYP</h1>
+          <div class="header">
+      <form @submit.prevent="Submit" class="form">
+        <div class="form1">
+          <label for="namee">Comment</label>
+          <input type="text" required v-model="user.comment" id="comment" class="form2">
+        </div>
+        <button type="submit" class="button">Submit</button>
+        </form>
+          </div>
       </div>
   </template>
   
-  <script setup>
+  <script>
   import { RouterLink } from 'vue-router'
+  import { supabase } from '@/lib/supabaseClient.js'
+
+  export default {
+    data() {
+      return {
+        users: [],
+  
+        user: {
+          Username: '',
+          Password: '',
+        }
+      };
+    },
+    methods: {
+  
+      Submit() {
+        this.users.push(this.user)
+        this.user = {Username:'', Password:'',};
+        console.log(this.users)
+        this.users.forEach((user) => {
+        supabase.from('Users').insert([user])
+            .then(({ data, error }) => {
+                if (error) {
+                    console.error(error.message);
+                } else {
+                    console.log(data);
+                }
+            });
+    });
+    
+  },
+    }
+  }
+
   </script>
   
   <style >
