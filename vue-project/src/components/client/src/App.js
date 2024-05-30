f8354cad3f874be3e9add03c07d480555d6479ef
juuclue <144699439+juuclue@users.noreply.github.com>
2024-05-30 09:30:36 -0400

    Update App.js

--- vue-project/src/components/client/src/App.js
+++ vue-project/src/components/client/src/App.js
@@ -1,15 +1,33 @@
 import logo from './logo.svg';
 import { useState, useEffect} from 'react';
 import './App.css';
-import { Container, Form, Button } from 'react-bootstrap'
+import { Container, Form, Button, Row, Col, Card} from 'react-bootstrap'
 import 'bootstrap/dist.css/bootstrap/min.css'
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react"
+import {v4 as uuidv4 } from 'uuid';
 
+const CDNURL ="https://givenxncncbjahyqmfpn.supabase.co/storage/v1/object/public/avatar"
 function App() {
   const [email, setEmail ] = useState("")
+  const [ images, setImages] = useState([]);
   const user = useUser();
   const supabase = useSupabaseClient();
 
+  async function getImages() {
+    const { data, error} = await supabase
+    .storagefrom('images')
+    .list(user?.id + "/", {
+      limit: 100,
+      offset: 0,
+      sortBy: { column: "name", order: "asc"}
+    });
+
+    if(data !== null ) {
+setImages(data);
+    } else{
+      alert("Error")
+    }
+  }
   async function magicLinkLogin() {
     const { data, error} = await supabase.auth.signInWithOtp({
       email: email
@@ -24,13 +42,31 @@ function App() {
   async function signOut() {
     const { error } = await supabase.auth.signOut();
   }
+  
+  async function uploadImage(e) {
+    let file = e.target.files[0];
+
+    const { data, error } = await supabase
+    .storage
+    .from('images')
+    .uploadImage(user.id + "/" + uuidv4(), file)
+
+    if(data) {
+      getImages();
+
+    }else {
+      console.log(error);
+    }
   }
+
+
   return (
     <Container align="center" className="container-sm mt-4">
       {/*
       if user exists: show them the images / upload images page
     if they dont exist: show them the login page
       */}
+
   { user === null ?
   <>
   <h1>Welcome to ImageWall</h1>
@@ -55,13 +91,19 @@ function App() {
   <h1> Your ImageWall</h1>
   <Button onClick={() => signOut()}> Sign Out</Button>
   <p>Current user: {user.email}</p>
+  <p>use choose file button to upload image</p>
+  <Form.Group className="mb-3" style={{maxWidth: "500px"}}>
+    <Form.Control type ="file" accpet ="image/png, image/jpeg" onChange={(e) => uploadImage(e)}/>
+
+  </Form.Group>
   <hr />
-  <h3> </h3>
+  <h3> Your Images </h3>
+  <Row xs =
   </>
   }
     </Container>
   );
   
-
+}
 
 export default App;
