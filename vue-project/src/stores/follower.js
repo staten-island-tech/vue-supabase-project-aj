31e2bff94576a70b4e89a62971178d03d47ada81
amyl164 <amyl81008@gmail.com>
2024-06-06 18:58:04 -0400

    ds

--- vue-project/src/stores/follower.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia';
-import {ref} from 'vue'
-
-export let followStore = defineStore('followStore', () => {
-  const isUserFollowed = ref(false)
-
-
-  return {isUserFollowed}
-})
